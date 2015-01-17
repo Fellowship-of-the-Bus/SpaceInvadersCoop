c6b588d88c40c86e09b33e68323772b7b2e8abf6
Rob Schluntz <saitou1024@gmail.com>
2015-01-17 11:49:47 -0500

    basic directory structure

--- src/main/scala/spaceInvader/SpaceInvader.scala
+++ src/main/scala/spaceInvader/SpaceInvader.scala
@@ -1,14 +1,14 @@
-package simpleslickgame
+package spaceInvader
 import java.util.logging.{Level, Logger}
 import org.newdawn.slick.{AppGameContainer, BasicGame, GameContainer, Graphics, SlickException}
 
-class SimpleSlickGame(gamename: String) extends BasicGame(gamename) {
+class SpaceInvader(gamename: String) extends BasicGame(gamename) {
   override def init(gc: GameContainer) = gc.setShowFPS(true) 
   override def update(gc: GameContainer, i: Int) = {}
   override def render(gc: GameContainer, g: Graphics) = g.drawString("Hi!", 10, 10)
 }
 
-object SimpleSlickGame extends App {
+object SpaceInvader extends App {
   try {
     println("Library path is: " + System.getProperty("java.library.path"))
     val appgc = new AppGameContainer(new SimpleSlickGame("Simple Slick Game"))
