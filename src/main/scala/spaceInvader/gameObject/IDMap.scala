ea62f1c6f008257388d66d36644402c252c96bf2
Rob Schluntz <saitou1024@gmail.com>
2015-01-24 18:37:16 -0500

    power up ids

--- src/main/scala/spaceInvader/gameObject/IDMap.scala
+++ src/main/scala/spaceInvader/gameObject/IDMap.scala
@@ -4,20 +4,22 @@ import spaceInvader.SpaceInvader._
 object IDMap{
   val playerID = 1
 
-  val EnemyStart = 10
-  val DroneID = 10
-  val FighterID = 11
-  val CosmicBeeID = 12
-  val SpaceTurtleID = 13
-  val GalacticDragonID = 14
-  val EnemyEnd = 15
+  val EnemyStart = 11
+  val DroneID = 11
+  val FighterID = 12
+  val CosmicBeeID = 13
+  val SpaceTurtleID = 14
+  val GalacticDragonID = 15
+  val EnemyEnd = 16
 
   val BulletID: Int  = 101
   val MissileID: Int = 102
   val PBulletID = 151
 
+  val StartPowerUp = 201
   val PowerHPID = 201
   val PowerShotsID = 202
+  val EndPowerUp = 203
 
   val GameOverID = 1001
   val HeartID = 1002
