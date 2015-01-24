ec4b0039a2cb5a0c40342c765d46a8f242fddf8a
Rob Schluntz <saitou1024@gmail.com>
2015-01-24 18:12:59 -0500

    Power Up IDs and images

--- src/main/scala/spaceInvader/gameObject/IDMap.scala
+++ src/main/scala/spaceInvader/gameObject/IDMap.scala
@@ -2,47 +2,48 @@ package spaceInvader.gameObject
 import spaceInvader.SpaceInvader._
 
 object IDMap{
-    val playerID = 1
+  val playerID = 1
 
-    val EnemyStart = 10
-    val DroneID = 10
-    val FighterID = 11
-    val CosmicBeeID = 12
-    val SpaceTurtleID = 13
-    val GalacticDragonID = 14
-    val EnemyEnd = 15
+  val EnemyStart = 10
+  val DroneID = 10
+  val FighterID = 11
+  val CosmicBeeID = 12
+  val SpaceTurtleID = 13
+  val GalacticDragonID = 14
+  val EnemyEnd = 15
 
-    val BulletID: Int  = 101
-    val MissileID: Int = 102
-    val PBulletID = 151
+  val BulletID: Int  = 101
+  val MissileID: Int = 102
+  val PBulletID = 151
 
-    val Power1ID = 201
+  val PowerHPID = 201
+  val PowerShotsID = 202
 
-    val GameOverID = 1001
-    val HeartID = 1002
+  val GameOverID = 1001
+  val HeartID = 1002
 
-    val imageMap = Map(
-        playerID -> "img/PlayerR.png",
+  val imageMap = Map(
+    playerID -> "img/PlayerR.png",
 
-        DroneID -> "img/Drone.png",
-        FighterID -> "img/Fighter.png",
-        CosmicBeeID -> "img/CosmicBee.png",
-        SpaceTurtleID -> "img/SpaceTurtle.png",
-        GalacticDragonID -> "img/GalacticDragon.png",
+    DroneID -> "img/Drone.png",
+    FighterID -> "img/Fighter.png",
+    CosmicBeeID -> "img/CosmicBee.png",
+    SpaceTurtleID -> "img/SpaceTurtle.png",
+    GalacticDragonID -> "img/GalacticDragon.png",
 
+    BulletID -> "img/Bullet.png",
+    MissileID -> "img/Missile.png",
+    PBulletID -> "img/PBullet.png",
 
-        BulletID -> "img/Bullet.png",
-        // MissileID -> "img/Missile.png",
-        PBulletID -> "img/PBullet.png",
+    PowerHPID -> "img/PowerHP.png",
+    PowerShotsID -> "img/PowerShots",
 
-        // Power1ID -> "img/player.png"
+    GameOverID -> "img/GameOver.png",
+    HeartID -> "img/Heart.png"
+  )
 
-        GameOverID -> "img/GameOver.png",
-        HeartID -> "img/Heart.png"
-    )
-
-    lazy val images = imageMap.map { x =>
-      val (id, loc) = x
-      id -> makeImg(loc)
-    }
+  lazy val images = imageMap.map { x =>
+    val (id, loc) = x
+    id -> makeImg(loc)
+  }
 }
